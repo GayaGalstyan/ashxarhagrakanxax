8299bc698d828415ee320ec98c137c8dfb0c7f04
GayaGalstyan <gayane.galstyan1@tumo.org>
2023-07-06 17:57:14 +0400

    06.07

---
+++ countries.js
@@ -0,0 +1,89 @@
+const countries = [
+    {
+        "flag": "flags/KD.png ",
+        "name": "Մեծ Բրիտանիա"
+    },
+    {
+        "flag": "flags/Australia.svg.png ",
+        "name": "Ավստրալիա "
+    },
+    {
+        "flag": "flags/Georgia.svg.png ",
+        "name": "Վրաստան "
+    },
+    {
+        "flag": "flags/Germany.svg.png ",
+        "name": "Գերմանիա"
+    },
+    {
+        "flag": "flags/Turkey.svg.png ",
+        "name": "Թուրքիա "
+    },
+   
+    {
+        "flag": "flags/Rus.jpeg ",
+        "name": "Ռուսաստան"
+    },
+    {
+        "flag": "flags/Sk.svg.png ",
+        "name": "Հարավային Կորեա "
+    },
+    {
+        "flag": "flags/Ar.svg.png ",
+        "name": "Արգենտինա"
+    },
+    {
+        "flag": "flags/Canada.svg.png ",
+        "name": "Կանադա"
+    },
+    {
+        "flag": "flags/EU-France.svg.png ",
+        "name": "Ֆրանսիա "
+    },
+    {
+        "flag": "flags/Japan.svg.png ",
+        "name": "Ճապոնիա"
+    },
+    {
+        "flag": "flags/MEX.svg.png ",
+        "name": "Մեքսիկա "
+    },
+    {
+        "flag": "flags/downland.png ",
+        "name": "Իտալիա "
+    },
+    {
+        "flag": "flags/images.jpeg ",
+        "name": "Հայաստան "
+    },
+    {
+        "flag": "flags/Amn.jpeg ",
+        "name": "Ամն "
+    },
+    {
+        "flag": "flags/Hah.svg.png ",
+        "name": "ՀԱՀ "
+    },
+   
+    {
+        "flag": "flags/Brazil.svg.png ",
+        "name": "Բռազիլիա "
+    },
+    {
+        "flag": "flags/Chile.svg.png ",
+        "name": "Չիլի "
+    },
+    {
+        "flag": "flags/CHN.svg.png ",
+        "name": "Չինաստան"
+    },
+    {
+        "flag": "flags/Eg.svg.png ",
+        "name": "Եգիպտոս"
+    },
+    {
+        "flag": "flags/Hk.svg.png ",
+        "name": "Հյուսիսային Կորեա "
+    },
+
+]
--- main.js
+++ main.js
@@ -1,36 +1,65 @@
+let correct;
 let seconds = 10;
 let correctAnswer = 0;
 let incorrectAnswer = 0;
 
 function getElement(id) {
-    return document.getElementById(id);
+  return document.getElementById(id);
 }
+function getRandomCountry() {
+  return countries[Math.floor(Math.random(countries.length - 1) * 10)]
+}
+function main() {
+  let options = [];
+  const maxOptions = 3;
+  while (options.length < maxOptions) {
+    let coun = getRandomCountry();
+    if (options.indexOf(coun) === -1) {
+      options.push(coun);
+    }
+
+  }
+  for (let i = 0; i < options.length; i++) {
+    getElement(`option${i + 1}label`).innerHTML = options[i].name;
+    getElement(`option${i + 1}label`).value = options[i].name;
+    getElement(`option${i + 1}label`).checked = false;
+  }
+  correct = options[Math.round(Math.random() * (options.length - 1))];
+  getElement("bcolor").src = correct.flag;
+}
+
 function timer() {
-    getElement("time").innerHTML = seconds;
-    setTimeout(finish, seconds * 1000);
-    let countdown = setInterval(function () {
-        seconds--;
-        getElement("time").textContent = seconds;
-        if (seconds <= 0) clearInterval(countdown);
-        if (seconds === 5) getElement("time").style.color = "#ff0000";
-    }, 1000);
+  getElement("time").innerHTML = seconds;
+  setTimeout(finish, seconds * 1000);
+  let countdown = setInterval(function () {
+    seconds--;
+    getElement("time").textContent = seconds;
+    if (seconds <= 0) clearInterval(countdown);
+    if (seconds === 5) getElement("time").style.color = "#ff0000";
+  }, 1000);
 }
 function check() {
-    let input;
-    try {
-      input = document.querySelector('input[name = "option"]:checked').value;
-    } catch {
-      return;
-    }
+  let input;
+  try {
+    input = document.querySelector('input[name = "option"]:checked').value;
+  } catch {
+    return;
+  }
+  if (input === correct.name) {
     correctAnswer++;
     getElement("score").innerHTML = correctAnswer;
-     clearInterval(checkInterval);
+
+  } else {
+    incorrectAnswer++;
   }
-  function finish() {
-     clearInterval(checkInterval);
-    let percentage = 100;
-    getElement("alertaccuracy").innerHTML = `der ardyunqn e ${percentage}%`;
-  }
-  let checkInterval = setInterval(check, 50);
-  
+  main();
+
+}
+function finish() {
+  clearInterval(checkInterval);
+  let percentage = correctAnswer * 100 / incorrectAnswer;
+  getElement("alertaccuracy").innerHTML = `${percentage}%`;
+}
+let checkInterval = setInterval(check, 50);
+main();
 timer();
